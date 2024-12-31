6da81a06849b9ed8e417694894a06454a3122f64
im2back <jefferson.richards2@outlook.com>
2024-12-31 10:17:52 -0300

    Ajusta nas consultas de sumario, pra incluir vendedores sem venda - caso adicionado

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -15,16 +15,16 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
 	
 	@Query("""
 		    SELECT new com.devsuperior.dsmeta.dto.SumarioVendasPorVendedorDTO(
-		        s.name, SUM(sa.amount))
-		    FROM Sale sa
-		    JOIN sa.seller s
-		    WHERE sa.date BETWEEN :minDate AND :maxDate
+		        s.name, COALESCE(SUM(sa.amount), 0))
+		    FROM Seller s
+		    LEFT JOIN s.sales sa
+		    ON sa.date BETWEEN :minDate AND :maxDate
 		    GROUP BY s.name
 		""")
 		List<SumarioVendasPorVendedorDTO> getSummary(
-		    @Param("minDate") LocalDate minDate, 
+		    @Param("minDate") LocalDate minDate,
 		    @Param("maxDate") LocalDate maxDate
 		);
 
-
+	
 }
